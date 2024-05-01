53d2a110efcb488624e9187fa6cdced87e799afd
chacha <chacha@sparcs.org>
2024-05-01 15:25:34 +0900

    tensor done

--- numpy-tutorial.py
+++ numpy-tutorial.py
@@ -40,10 +40,58 @@ x_np = torch.from_numpy(np_array) # ndarray to tensor
 x_ones = torch.ones_like(x_data) # x_data의 shape, type 을 유지하면서 1 행렬을 생성
 print(f"Ones Tensor: \n {x_ones} \n")
 
-x_rand = torch.rand_like(x_data, dtype=torch.float) # x_data의 type 을 덮어쓴다. dtype 을 따로 정의하기 때문에
+x_rand = torch.rand_like(x_data, dtype = torch.float) # x_data의 type 을 덮어쓴다. dtype 을 따로 정의하기 때문에
 print(f"Random Tensor: \n {x_rand} \n")
 
-# 무작위(random) 또는 상수(constant) 값을 사용하기: 부터 !
+print(x_data[:, 0]) # x_data 의 0, 1 column 출력
+# tensor 도 기본 행렬처럼 !!!
+
+x_data1 = torch.cat([x_data, x_data, x_data], dim = 1) # 1차원으로 옆으로 concat
+print(x_data1)
+
+x_data2 = torch.stack((x_data, x_data, x_data), dim = 0) # stack 의 특징을 알아보자
+print(x_data2)
+
+x_data3 = torch.stack((x_data, x_data, x_data), dim = 1) 
+print(x_data3)
+
+x_data4 = torch.stack((x_data, x_data, x_data), dim = 2) 
+print(x_data4)
+
+# 아래 두 코드가 어떤 텐서의 transpose 를 주는 코드
+y1 = x_data.T
+print(y1)
+
+y2 = x_data.matmul(x_data.T) # 자기 자신과 transpose 를 곱하기
+print(y2)
+
+y3 = x_data.sum() 
+# 텐서 안의 모든 요소의 합을 산출해서 요소 1개짜리 텐서를 반환
+# 해당 텐서는 요소가 하나이므로 item() 함수를 쓰면 python 의 숫자 값으로 변환 가능
+print(y3.item())
+
+x_data.add_(2) 
+# _ underscore 가 의미하는 것은 x_data 의 모든 요소에 2를 더한 다음 x_data를 아예 바꿔버림
+# _ 없으면 원래 거를 바꾸지 않고 그냥 더한 상태의 새로운 텐서를 내 놓는다
+print(x_data)
+
+# *** 만약 텐서를 넘파이로 변환한다면
+numpy_x = x_data.numpy()
+
+x_data.add_(3) # 이걸 하면 이거에 참조되어 변환된 넘파이 엑스도 같이 바뀐다
+
+print(x_data)
+print(numpy_x) # 같이 바뀐 넘파이가 출력됨
+
+# *** 만약 넘파이를 텐서로 변환한다면
+numpy_y = np.ones(5)
+y_data = torch.from_numpy(numpy_y) # from numpy, torch 를 만든다.
+
+np.add(numpy_y, 1, out = numpy_y) # numpy_y 에 1을 더해서 그 결과를 다시 out 에 저장
+print(y_data) # 상응하는 텐서도 같이 바뀜
+
+
+
 
 
 
